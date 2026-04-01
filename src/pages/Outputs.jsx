e510b83bb8251ef8301904deec1bb3a70c8b5424
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-04-01 20:17:49 +0530

    image and category divided in gallery.js and mapped in outputs.jsx

--- src/pages/Outputs.jsx
+++ src/pages/Outputs.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import { gallery } from "../data/gallery";
 
 const Outputs = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="p-6">
+      {Object.keys(gallery).map((category) => (
+        <div key={category}>
+          <h2 className="text-xl font-bold mb-4 capitalize">{category}</h2>
+
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {gallery[category].map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`${category} ${index}`}
+                className="w-full h-60 object-cover rounded-lg shadow-md hover:scale-105 transition duration-300"
+              />
+            ))}
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Outputs;
