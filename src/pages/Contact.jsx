d9f95da02bcb11d035d36407dc8482388b533215
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-04-03 12:06:06 +0530

    sample contact added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <a href="https://wa.me/918940695948" target="_blank">
+        <button className="bg-green-500 text-white p-3 rounded">
+          WhatsApp Us
+        </button>
+      </a>
+    </div>
+  );
 };
 
 export default Contact;
